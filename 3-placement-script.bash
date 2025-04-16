a0195e5d699e5c8e3c4bebbfb547ef2c5ea9cda2
kbuor <77895173+kbuor@users.noreply.github.com>
2025-04-16 13:38:47 +0700

    Create 3-placement-script.bash

---
+++ 3-placement-script.bash
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+mysql << EOF
+CREATE DATABASE placement;
+GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'localhost' IDENTIFIED BY 'Passw0rd';
+GRANT ALL PRIVILEGES ON placement.* TO 'placement'@'%' IDENTIFIED BY 'Passw0rd';
+EXIT;
+EOF
+export OS_USERNAME=admin
+export OS_PASSWORD=Passw0rd
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+openstack user create --domain default --password 'Passw0rd' placement
+openstack role add --project service --user placement admin
+openstack service create --name placement --description "Placement API" placement
+openstack endpoint create --region RegionOne placement public http://controller:8778
+openstack endpoint create --region RegionOne placement internal http://controller:8778
+openstack endpoint create --region RegionOne placement admin http://controller:8778
+apt install placement-api -y
+cat <<'EOF' > /etc/placement/placement.conf
+[DEFAULT]
+[api]
+auth_strategy = keystone
+[cors]
+[keystone_authtoken]
+auth_url = http://controller:5000/v3
+memcached_servers = controller:11211
+auth_type = password
+project_domain_name = Default
+user_domain_name = Default
+project_name = service
+username = placement
+password = Passw0rd
+[oslo_middleware]
+[oslo_policy]
+[placement]
+[placement_database]
+connection = mysql+pymysql://placement:Passw0rd@controller/placement
+[profiler]
+EOF
+su -s /bin/sh -c "placement-manage db sync" placement
+systemctl enable apache2
+systemctl restart apache2
