22a9526707d6c13e54fa2294b6195b0644c77f5c
kbuor <77895173+kbuor@users.noreply.github.com>
2025-04-16 09:40:28 +0700

    Create 1-keystone-script.bash

---
+++ 1-keystone-script.bash
@@ -0,0 +1,126 @@
+#!/bin/bash
+
+# Ceate the database
+mysql
+CREATE DATABASE keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'Passw0rd';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'Passw0rd';
+
+# Install and configure Keystone
+apt install -y keystone
+cat <<EOF > /etc/keystone/keystone.conf
+[DEFAULT]
+log_dir = /var/log/keystone
+[application_credential]
+[assignment]
+[auth]
+[cache]
+[catalog]
+[cors]
+[credential]
+[database]
+connection = mysql+pymysql://keystone:Passw0rd@controller/keystone
+[domain_config]
+[endpoint_filter]
+[endpoint_policy]
+[extra_headers]
+Distribution = Ubuntu
+[federation]
+[fernet_receipts]
+[fernet_tokens]
+[healthcheck]
+[identity]
+[identity_mapping]
+[jwt_tokens]
+[ldap]
+[oauth1]
+[oauth2]
+[oslo_messaging_amqp]
+[oslo_messaging_kafka]
+[oslo_messaging_notifications]
+[oslo_messaging_rabbit]
+[oslo_middleware]
+[oslo_policy]
+[policy]
+[profiler]
+[receipt]
+[resource]
+[revoke]
+[role]
+[saml]
+[security_compliance]
+[shadow_users]
+[token]
+provider = fernet
+[tokenless_auth]
+[totp]
+[trust]
+[unified_limit]
+[wsgi]
+EOF
+
+su -s /bin/sh -c "keystone-manage db_sync" keystone
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap --bootstrap-password Passw0rd --bootstrap-admin-url http://controller:5000/v3/ --bootstrap-internal-url http://controller:5000/v3/ --bootstrap-public-url http://controller:5000/v3/ --bootstrap-region-id RegionOne
+
+
+# Configure the Apache HTTP server
+cat <<EOF > /etc/apache2/apache2.conf
+DefaultRuntimeDir ${APACHE_RUN_DIR}
+ServerName controller
+PidFile ${APACHE_PID_FILE}
+Timeout 300
+KeepAlive On
+MaxKeepAliveRequests 100
+KeepAliveTimeout 5
+User ${APACHE_RUN_USER}
+Group ${APACHE_RUN_GROUP}
+HostnameLookups Off
+ErrorLog ${APACHE_LOG_DIR}/error.log
+LogLevel warn
+IncludeOptional mods-enabled/*.load
+IncludeOptional mods-enabled/*.conf
+Include ports.conf
+<Directory />
+        Options FollowSymLinks
+        AllowOverride None
+        Require all denied
+</Directory>
+<Directory /usr/share>
+        AllowOverride None
+        Require all granted
+</Directory>
+<Directory /var/www/>
+        Options Indexes FollowSymLinks
+        AllowOverride None
+        Require all granted
+</Directory>
+AccessFileName .htaccess
+<FilesMatch "^\.ht">
+        Require all denied
+</FilesMatch>
+LogFormat "%v:%p %h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"" vhost_combined
+LogFormat "%h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\"" combined
+LogFormat "%h %l %u %t \"%r\" %>s %O" common
+LogFormat "%{Referer}i -> %U" referer
+LogFormat "%{User-agent}i" agent
+IncludeOptional conf-enabled/*.conf
+IncludeOptional sites-enabled/*.conf
+EOF
+
+systemctl enable apache2
+systemctl restart apache2
+
+cat <<EOF > admin-openrc
+export OS_USERNAME=admin
+export OS_PASSWORD=Passw0rd
+export OS_PROJECT_NAME=admin
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_AUTH_URL=http://controller:5000/v3
+export OS_IDENTITY_API_VERSION=3
+EOF
+
+sources admin-openrc
+openstack project create --domain default --description "Service Project" service
