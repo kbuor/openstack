acc0ce71d955f3086dbdce9cff675e9d34cbc1c0
kbuor <77895173+kbuor@users.noreply.github.com>
2025-04-16 09:06:16 +0700

    Create script.bash

---
+++ script.bash
@@ -0,0 +1,79 @@
+#!/bin/bash
+# Prepare OS
+echo '10.0.0.11 controller' >> /etc/hosts
+echo '10.0.0.31 compute1' >> /etc/hosts
+apt update -y && apt upgrade -y
+apt install python3-openstackclient -y
+
+# Install Chrony
+apt install chrony -y
+cat <<EOF > /etc/chrony/chrony.conf
+confdir /etc/chrony/conf.d
+sourcedir /run/chrony-dhcp
+sourcedir /etc/chrony/sources.d
+keyfile /etc/chrony/chrony.keys
+driftfile /var/lib/chrony/chrony.drift
+ntsdumpdir /var/lib/chrony
+logdir /var/log/chrony
+maxupdateskew 100.0
+rtcsync
+makestep 1 3
+leapsectz right/UTC
+server 0.vn.pool.ntp.org iburst
+allow 10.0.0.0/24
+EOF
+
+systemctl enable chronyd
+systemctl restart chronyd
+
+# Install SQL Database
+apt install mariadb-server python3-pymysql
+cat <<EOF > /etc/mysql/mariadb.conf.d/99-openstack.cnf
+[mysqld]
+bind-address = 10.0.0.11
+
+default-storage-engine = innodb
+innodb_file_per_table = on
+max_connections = 4096
+collation-server = utf8_general_ci
+character-set-server = utf8
+EOF
+systemctl enable mysql
+systemctl restart mysql
+
+# Install Message Queue
+apt install -y rabbitmq-server
+rabbitmqctl add_user openstack Passw0rd
+rabbitmqctl set_permissions openstack ".*" ".*" ".*"
+systemctl enable rabbitmq-server
+systemctl restart rabbitmq-server
+
+# Install Memcached
+apt install -y memcached python3-memcache
+cat <<EOF > /etc/memcached.conf
+-d
+logfile /var/log/memcached.log
+-m 64
+-p 11211
+-u memcache
+-l 10.0.0.11
+-P /var/run/memcached/memcached.pid
+EOF
+systemctl enable memcached
+systemctl restart memcached
+
+# Install Etcd
+apt install -y etcd-server
+cat <<EOF > /etc/etcd/etcd.conf
+ETCD_NAME="controller"
+ETCD_DATA_DIR="/var/lib/etcd"
+ETCD_INITIAL_CLUSTER_STATE="new"
+ETCD_INITIAL_CLUSTER_TOKEN="etcd-cluster-01"
+ETCD_INITIAL_CLUSTER="controller=http://10.0.0.11:2380"
+ETCD_INITIAL_ADVERTISE_PEER_URLS="http://10.0.0.11:2380"
+ETCD_ADVERTISE_CLIENT_URLS="http://10.0.0.11:2379"
+ETCD_LISTEN_PEER_URLS="http://0.0.0.0:2380"
+ETCD_LISTEN_CLIENT_URLS="http://10.0.0.11:2379"
+EOF
+systemctl enable etcd
+systemctl restart etcd
